89f0f97a0bb589f579279eb08ec5d9ce24d41743
Mohammad Kasim <ka000sim@gmail.com>
2025-02-02 09:26:08 +0530

    Add files via upload

---
+++ printcountdownno.py
@@ -0,0 +1,7 @@
+# user input
+number = int(input("Enter a number to start the countdown: "))
+# Countdown using a while loop
+while number >= 0:
+    print(number)
+    number -= 1  # Decrement the number
+print("Countdown complete!")
